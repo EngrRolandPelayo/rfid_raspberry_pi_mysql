1a9079422ed1eaae98d8b0bb69feef0e70f8f578
kurimawxx00 <kurimawxx00@gmail.com>
2018-04-17 10:50:22 +0800

    Edited MFRC522.py

--- Login.py
+++ Login.py
@@ -38,8 +38,9 @@ signal.signal(signal.SIGINT, end_read)
 MIFAREReader = MFRC522.MFRC522()
 
 # Welcome message
-#print "Welcome to the MFRC522 data read example"
-#print "Press Ctrl-C to stop."
+print "Welcome to the Raspberry Pi RFID Login System"
+print "Tap the card now."
+print "Press Ctrl-C to exit."
 
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
 while continue_reading:
